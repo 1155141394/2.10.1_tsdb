a81dd791b81afb225ca071af8c6a387f1a895787
taozhisheng <1155141394@link.cuhk.edu.hk>
2023-04-05 14:12:56 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/PythonFile/map_matrix.py

--- src/planner/get_parameter.c
+++ src/planner/get_parameter.c
@@ -15,14 +15,13 @@ query_by_python(char* attrs, char* table, char* where_clause);
 void
 query_by_python(char* attrs, char* table, char* where_clause){
     // Initialize Python interpreter
-    fprintf(stderr, "Start query function.\n");
     Py_Initialize();
-
+    fprintf(stderr, "Start query function.\n");
     // Build the name object for the module and function
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('/var/lib/postgresql/2.10.1_tsdb/src/PythonFile')");
 
-    fprintf(stderr, "Start query function.\n");
+
     // Import the module
     PyObject* pmodule = PyImport_ImportModule("query");
     if (!pmodule)
