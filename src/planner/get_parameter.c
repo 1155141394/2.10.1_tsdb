da15f72a7223f096f247325ba4de26f36178c730
xutao <464612404@qq.com>
2023-04-05 13:51:52 +0800

    add pthread detach

--- src/planner/get_parameter.c
+++ src/planner/get_parameter.c
@@ -16,7 +16,7 @@ void
 query_by_python(char* attrs, char* table, char* where_clause){
     // Initialize Python interpreter
     fprintf(stderr, "Start query function.\n");
-    Py_Initialize();
+//    Py_Initialize();
 
     // Build the name object for the module and function
     PyRun_SimpleString("import sys");
