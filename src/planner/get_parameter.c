68b3d8d2a2d77ab1cea79fc0e2c24639cf5ae34d
xutao <464612404@qq.com>
2023-04-05 13:48:40 +0800

    add pthread detach

--- src/planner/get_parameter.c
+++ src/planner/get_parameter.c
@@ -15,8 +15,9 @@ query_by_python(char* attrs, char* table, char* where_clause);
 void
 query_by_python(char* attrs, char* table, char* where_clause){
     // Initialize Python interpreter
-    Py_Initialize();
     fprintf(stderr, "Start query function.\n");
+    Py_Initialize();
+
     // Build the name object for the module and function
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('/var/lib/postgresql/2.10.1_tsdb/src/PythonFile')");
